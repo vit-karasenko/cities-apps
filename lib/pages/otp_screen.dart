38d620d245ba7867dd9336cfdcfc317a20d2cb18
vit_mit <vit.karasenko@gmail.com>
2023-12-06 03:01:48 +0200

    add чтото

--- lib/pages/otp_screen.dart
+++ lib/pages/otp_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'done.dart';
-import 'name_request_screen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class OTPScreen extends StatefulWidget {
   final String verificationId;
@@ -50,39 +48,10 @@ class _OTPScreenState extends State<OTPScreen> {
         smsCode: _otpController.text.trim(),
       );
       await _auth.signInWithCredential(authCredential);
-      await _checkAndNavigate();
-    } catch (e) {
-      _showErrorDialog(e.toString());
-    }
-  }
-
-  Future<void> _checkAndNavigate() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? userName = prefs.getString('userName');
-    if (userName == null || userName.isEmpty) {
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => NameRequestScreen()));
-    } else {
       Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (context) => Done()));
+    } catch (e) {
+      print(e);
     }
-  }
-
-  void _showErrorDialog(String message) {
-    showDialog(
-      context: context,
-      builder: (ctx) => AlertDialog(
-        title: Text('Ошибка'),
-        content: Text(message),
-        actions: <Widget>[
-          TextButton(
-            child: Text('ОК'),
-            onPressed: () {
-              Navigator.of(ctx).pop();
-            },
-          )
-        ],
-      ),
-    );
   }
 }
