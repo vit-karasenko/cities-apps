11775cabaf00ada811960fe6c3630a2ebea32e07
vit_mit <vit.karasenko@gmail.com>
2023-12-10 13:52:00 +0200

    add push teest2

--- lib/pages/settings_screen.dart
+++ lib/pages/settings_screen.dart
@@ -1,41 +1,29 @@
 import 'package:flutter/material.dart';
-import 'notification_service.dart';
+import 'notifications_screen.dart';
 
-class SettingsScreen extends StatefulWidget {
-  @override
-  _SettingsScreenState createState() => _SettingsScreenState();
-}
-
-class _SettingsScreenState extends State<SettingsScreen> {
-  bool curfewReminder = false;
-  final NotificationService _notificationService = NotificationService();
-
+class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Настройки'),
+        title: Text('Налаштування'),
       ),
       body: ListView(
         children: <Widget>[
-          SwitchListTile(
-            title: Text('Напоминание о комендантском времени'),
-            value: curfewReminder,
-            onChanged: (bool value) {
-              setState(() {
-                curfewReminder = value;
-              });
-              // Здесь можно добавить логику включения/выключения уведомлений
+          ListTile(
+            title: Text('Вийти'),
+            onTap: () {
+              // Здесь должен быть код для выхода из сессии
             },
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                _notificationService.showInstantNotification();
-              },
-              child: Text('Тестировать уведомление'),
-            ),
+          ListTile(
+            title: Text('Уведомления'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => NotificationsScreen()),
+              );
+            },
           ),
         ],
       ),
